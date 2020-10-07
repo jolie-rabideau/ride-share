32f2efacc743b214c423286079fec8891dc042b0
Jolie Rabideau <jolie_rabideau@tayloru.edu>
2020-10-06 21:30:08 -0400

    commit tues 9:30 pm jolie

--- api/db.js
+++ api/db.js
@@ -1,7 +1,7 @@
 const knex = require('knex')({
 	client: 'pg',
 	connection: {
-		host: '',
+		host: 'faradat.cse.taylor.edy',
 		user: '',
 		password: '',
 		database: ''
@@ -11,74 +11,5 @@ const objection = require('objection');
 const Model = objection.Model;
 Model.knex(knex);
 
-class Authorization extends Model {
-	static get tableName() {
-		return 'authorization';
-	}
-	static get relationMappings() {
-		return {
-			*otherTableName*: {
-				relation: ,
-				modelClass: ,
-				join: {
-					from: 'currentTable.otherTable_id',
-					to: 'otherTable.otherTable_id'
-				}
-			}
-		}
-	}
-}
+module.exports = { knex, Model };
 
-class Driver extends Model {
-	static get tableName() {
-		return 'driver';
-	}
-}
-
-class Drivers extends Model {
-	static get tableName() {
-		return 'drivers';
-	}
-}
-
-class Location extends Model {
-	static get tableName() {
-		return 'location';
-	}
-}
-
-class Passenger extends Model {
-	static get tableName() {
-		return 'passenger';
-	}
-}
-
-class Ride extends Model {
-	static get tableName() {
-		return 'ride';
-	}
-}
-
-class State extends Model {
-	static get tableName() {
-		return 'state';
-	}
-}
-
-class User extends Model {
-	static get tableName() {
-		return 'user';
-	}
-}
-
-class Vehicle_Type extends Model {
-	static get tableName() {
-		return 'vehicle_type';
-	}
-}
-
-class Vehicle extends Model {
-	static get tableName() {
-		return 'vehicle';
-	}
-}
--- api/models/Driver.js
+++ api/models/Driver.js
@@ -1,1 +1,43 @@
-created oct 6
+const { knex, Model } = require("../db");
+
+class Driver extends Model {
+	static get tableName() {
+		return 'driver';
+	}
+	static get relationalMappings() {
+		return {
+			user: {
+				relation: Model.HasManyRelation, 
+				modelClass: User,
+				join: {
+					from: 'user.id',
+					to: 'driver.userId'
+				}
+			}
+			authorization: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Authorization,
+				join: {
+					from: 'authorization.driverId',
+					to: 'driver.id'
+				}
+			}
+			Drivers: {
+				relation: Model.BelongsToOneRelation,
+				modelClass: Drivers,
+				join: {
+					from: 'drivers.driverId',
+					to: 'driver.id'
+				}
+			}
+			State: {
+				relation: Model.HasManyRelation,
+				modelClass: State,
+				join: {
+					from: 'state.abbreviation',
+					to: 'driver.licenseState'
+				}
+			}			
+		}	
+	}
+}
